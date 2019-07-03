2d3b8d47b020a451f4916285133a21a8384fd660
mrtyrdm66 <52512176+mrtyrdm66@users.noreply.github.com>
2019-07-04 02:06:59 +0300

    ilk gönderme

---
+++ js/swapi.js
@@ -0,0 +1,58 @@
+  var response;
+             var selected;
+
+             document.onreadystatechange = function() {
+                           if (document.readyState == 'complete') {
+                             pegar_personagens();
+                             document.querySelector('#starwars').onchange=monstrar_filmes;
+                           }
+                                        
+             }
+
+             function pegar_personagens() {
+                var httpRequest = new XMLHttpRequest();
+                          
+                 httpRequest.onreadystatechange = function() {
+            if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                response = JSON.parse(httpRequest.responseText);
+                var lista = document.querySelector('#starwars');
+                lista.innerHTML = '';
+
+                response.results.forEach(function(el){
+                               option = document.createElement('option');
+                               option.innerHTML = el.name;
+                               option.setAttribute('films', JSON.stringify(el.films))
+                               lista.appendChild(option);
+                })
+              }
+           }
+         }
+                           httpRequest.open('GET' , 'https://swapi.co/api/people/');
+                           httpRequest.send();    
+
+     
+             }
+             function monstrar_filmes(ev){
+                   selected = ev.target;
+                   films = JSON.parse(selected.selectedOptions[0].getAttribute('films'));
+                var lista = document.querySelector('#films');
+                lista.innerHTML = '';                   
+                    films.forEach(function(url){
+                     var httpRequest = new XMLHttpRequest();
+                     	httpRequest.onreadystatechange = function() {
+				            if (httpRequest.readyState === 4) {
+				            if (httpRequest.status === 200) {
+				  			film = JSON.parse(httpRequest.responseText);
+				  				li = document.createElement('li');
+				  				li.innerHTML = film.title;
+				  				lista.appendChild(li);
+				            		}
+				        		}
+				        		
+                     		}
+                     	httpRequest.open('GET' , url);
+                        httpRequest.send();
+ 
+                     	})
+                     } 
